102e877e9863ba68de91c4ced521b3e9a4e74271
swapnasruthi <swapnasruthi2005@gmail.com>
2025-03-19 15:03:38 +0530

    update app icon and add Search component with styling

--- app/(TABS)/index.tsx
+++ app/(TABS)/index.tsx
@@ -1,13 +1,27 @@
-import { Link } from "expo-router";
-import { Text, View } from "react-native";
+import { icons } from "@/constants/icons";
+import { images } from "@/constants/images";
+import { Link, useRouter } from "expo-router";
+import { Image, ScrollView, Text, View } from "react-native";
+import Search from "../components/Search";
 
 export default function Index() {
+  const router = useRouter();
   return (
     <View
-      className="flex-1 justify-center items-center"
+      className="flex-1 bg-primary"
     >
-      <Text className="text-5xl font-bold text-blue-500">Hello!</Text>
-      
+     <Image source={images.bg} className="absolute w-full z-0"/>
+      <ScrollView className="flex-1 px-5" showsVerticalScrollIndicator={false} contentContainerStyle={{minHeight:"100%", paddingBottom:10}}>
+        <Image source={icons.logo} className="w-12 h-10 mt-20 mb-5 mx-auto"/>
+        <View className="flex-1 mt-5">
+               <Search
+                    onPress = {() => router.push("./Search")}
+                    placeHolder="Search for any movie"
+               />
+           </View>
+      </ScrollView>
+
+           
     </View>
-  );
+  )
 }
