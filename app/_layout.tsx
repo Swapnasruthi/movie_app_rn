fbfbefdea7db8b2702cb8b83d9bc835b56d0b3db
swapnasruthi <swapnasruthi2005@gmail.com>
2025-03-28 16:49:02 +0530

    implement movie details screen; add MovieInfo component and fetch movie details from API

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,12 @@
 import { Stack } from "expo-router";
 import "./global.css";
+import { StatusBar } from "react-native";
 export default function RootLayout() {
-  return <Stack>
+  return (
+  <>
+      <StatusBar hidden={true}/>
+
+      <Stack>
       <Stack.Screen
           name="(TABS)"
           options={{
@@ -10,11 +15,13 @@ export default function RootLayout() {
       />
 
       <Stack.Screen 
-          name="./Movies/[id]"
+          name="Movies/[id]"
           options={{
             headerShown:false,
           }}
         />
       
   </Stack>;
+  </>
+  )
 }
